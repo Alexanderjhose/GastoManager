0818878b93f0e566590b93d46ee520486534b59d
Alexander <imalexanderjhose@gmail.com>
2024-08-23 15:59:47 -0400

    Creacion de la estructura, clase Usuario.

---
+++ src/main/java/com/example/demo/controlador/UsuarioControlador.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package com.example.demo.controlador;
+
+/**
+ * 
+ */
+public class UsuarioControlador {
+
+}
---
+++ src/main/java/com/example/demo/repositorio/UsuarioRepositorio.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package com.example.demo.repositorio;
+
+/**
+ * 
+ */
+public interface UsuarioRepositorio {
+
+}
