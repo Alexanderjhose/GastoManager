59d57b5b1af47f2aee61022330f2f4d6430fd6ee
Alexander <imalexanderjhose@gmail.com>
2024-08-24 11:06:35 -0400

    Creacion de metodo en la implementacion para crear los usuarios.

--- src/main/java/com/example/demo/servicio/UsuarioServicioImpl.java
+++ src/main/java/com/example/demo/servicio/UsuarioServicioImpl.java
@@ -3,9 +3,34 @@
  */
 package com.example.demo.servicio;
 
+import java.util.Objects;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.modelo.Usuario;
+import com.example.demo.repositorio.UsuarioRepositorio;
+
 /**
  * 
  */
-public class UsuarioServicioImpl {
+
+@Service
+public class UsuarioServicioImpl implements UsuarioServicio {
+
+	private final UsuarioRepositorio usuarioRepositorio;
+
+	public UsuarioServicioImpl(UsuarioRepositorio usuarioRepositorio) {
+		this.usuarioRepositorio = usuarioRepositorio;
+	}
+
+	@Transactional
+	@Override
+	public Usuario crearUsuario(Usuario usuario) {
+		if (!Objects.isNull(usuario.getId()) || !Objects.isNull(usuario.getNombre())) {
+			return usuarioRepositorio.save(usuario);
+		}
+		throw new IllegalArgumentException("El usuario no puede ser nulo.");
+	}
 
 }
