051226720775bf6b2786fa1b89e5f4c074957e6a
Alexander <imalexanderjhose@gmail.com>
2024-08-24 11:05:36 -0400

    Creando metodo para crear usuario

--- src/main/java/com/example/demo/servicio/UsuarioServicio.java
+++ src/main/java/com/example/demo/servicio/UsuarioServicio.java
@@ -3,9 +3,13 @@
  */
 package com.example.demo.servicio;
 
+import com.example.demo.modelo.Usuario;
+
 /**
  * 
  */
 public interface UsuarioServicio {
+
+	public Usuario crearUsuario(Usuario usuario);
 
 }
